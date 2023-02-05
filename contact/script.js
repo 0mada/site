06bfdffcfa5bb26d43f63573dea8752808d92b64
Fluffery <74445505+flufferyxd@users.noreply.github.com>
2023-02-05 15:29:01 -0500

    Delete script.js

--- contact/script.js
+++
@@ -1,6 +0,0 @@
-function h(evt){evt.preventDefault()
-window.removeEventListener('touchstart',h,null)
-window.removeEventListener('click',h,null)
-const song=new Audio('song.mp3');document.querySelector('.before').remove();document.querySelector('.after').style.display='block';song.play();}
-window.addEventListener('touchstart',h)
-window.addEventListener('click',h)
